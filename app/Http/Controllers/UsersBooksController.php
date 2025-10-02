5d802bb242a58b8380ae9ef8fc8b40981467bb2a
BrizaDoVento <eikemontezuma@gmail.com>
2025-10-02 12:01:43 -0300

    commit do dia 02-10

---
+++ app/Http/Controllers/UsersBooksController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use illuminte\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Book;
+use App\Models\UsersBooks;
+
+class UsersBookController extends Controller
+{
+    //
+}
--- app/Models/UsersBooks.php
+++ app/Models/UsersBooks.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class UsersBooks extends Model
 {
-    use HasFactory;
+    protected $table ='users_books';
+    protected $fillable =['user_id','book_id','status_id','start_date','end_date'];
+
+    public function user() { return $this->belongsTo(User::class); }
+    public function book() { return $this->belongsTo(Book::class); }
 }

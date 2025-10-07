8c227e936fe940b302dc8625972c44a426795af0
BrizaDoVento <eikemontezuma@gmail.com>
2025-10-07 11:56:22 -0300

    Backup 07-10

--- app/Http/Controllers/UsersBooksController.php
+++ app/Http/Controllers/UsersBooksController.php
@@ -10,5 +10,18 @@ use App\Models\UsersBooks;
 
 class UsersBookController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'book_id' => 'required|exissts:books,id',
+        ]);
+
+        $user =
+        User::findOrFail($request->user_id);
+        $bookid = $request->book_id;
+
+        // Conta empréstimos atvios
+        $ativos = UsersBooks::where('user_id', )
+    }
 }

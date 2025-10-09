6bb67c900d9e5326805d7200d6fafcb4b608c29b
BrizaDoVento <eikemontezuma@gmail.com>
2025-10-08 23:30:50 -0300

    Update 2025_10_01_124952_create_users_books_table.php

--- database/migrations/2025_10_01_124952_create_users_books_table.php
+++ database/migrations/2025_10_01_124952_create_users_books_table.php
@@ -4,29 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersBooksTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+return new class extends Migration {
+    public function up(): void
     {
         Schema::create('users_books', function (Blueprint $table) {
             $table->id();
-            $table->foreign('user_id')->constrained('users');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('book_id')->constrained('books');
+            $table->foreignId('status_id')->constrained('users_books_status');
+            $table->date('start_date');
+            $table->date('end_date');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users_books');
     }
-}
+};

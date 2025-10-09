a6bae501507537d3d652f5ab9d80912542b17f20
BrizaDoVento <eikemontezuma@gmail.com>
2025-10-08 23:35:45 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersBookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/loans', [UsersBookController::class, 'store'])->name('loans.store');
+Route::post('/loans/{id}/return', [UsersBookController::class, 'devolver'])->name('loans.return');
+Route::get('/loans/overdue', [UsersBookController::class, 'atrasados'])->name('loans.overdue');
